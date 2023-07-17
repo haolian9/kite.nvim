20ff453c82d1ca4f0215b97d70a7b0e4544c6482
haoliang <haoliang0.1.2@gmail.com>
2023-07-17 22:41:37 +0800

    sync with upstream

--- lua/kite/facts.lua
+++ lua/kite/facts.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local highlighter = require("infra.highlighter")
+
 local api = vim.api
 
 M.totem = "kite"
@@ -7,8 +9,14 @@ M.max_children = 499
 
 do
   M.hl_ns = api.nvim_create_namespace("kite")
-  api.nvim_set_hl(M.hl_ns, "NormalFloat", { ctermbg = 15, ctermfg = 8 })
-  api.nvim_set_hl(M.hl_ns, "WinSeparator", { ctermfg = 243 })
+  local hi = highlighter(M.hl_ns)
+  if vim.go.background == "light" then
+    hi("NormalFloat", { fg = 8 })
+    hi("WinSeparator", { fg = 243 })
+  else
+    hi("NormalFloat", { fg = 7 })
+    hi("WinSeparator", { fg = 243 })
+  end
 end
 
 return M
